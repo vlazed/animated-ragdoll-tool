3bb1d932e859d2f5705734dd30b03a6af9055034
vlazed <dinoboy.valdez4@gmail.com>
2024-09-19 08:55:12 -0500

    Make a global table to store player puppets and puppeteers; add playback convar

--- lua/autorun/ragdollpuppeteer.lua
+++ lua/autorun/ragdollpuppeteer.lua
@@ -5,6 +5,34 @@ if SERVER then
 	AddCSLuaFile("ragdollpuppeteer/smh.lua")
 	AddCSLuaFile("ragdollpuppeteer/ui.lua")
 
+	---@type RagdollPuppeteerPlayerField[]
+	RAGDOLLPUPPETEER_PLAYERS = {}
+
+	gameevent.Listen("player_connect")
+	hook.Add("player_connect", "ragdollpuppeteer_PlayerConnect", function(data)
+		local userId = data.userid
+		RAGDOLLPUPPETEER_PLAYERS[userId] = {
+			player = Player(userId),
+			puppet = NULL,
+			puppeteer = NULL,
+			physicsCount = 0,
+			currentIndex = 0,
+			cycle = 0,
+			fps = 30,
+		}
+		print(userId, "connected!")
+		PrintTable(RAGDOLLPUPPETEER_PLAYERS[userId])
+	end)
+
+	gameevent.Listen("player_disconnect")
+	hook.Add("player_disconnect", "ragdollpuppeteer_PlayerDisconnect", function(data)
+		local userId = data.userId
+		timer.Remove("ragdollpuppeteer_playback_" .. tostring(userId))
+		RAGDOLLPUPPETEER_PLAYERS[userId] = nil
+		print(userId, "disconnected!")
+		PrintTable(RAGDOLLPUPPETEER_PLAYERS[userId])
+	end)
+
 	resource.AddSingleFile("resource/localization/en/ragdollpuppeteer_tool.properties")
 	resource.AddSingleFile("resource/localization/en/ragdollpuppeteer_ui.properties")
 end
