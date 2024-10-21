3b5ee84bbe7a1400a0faa57527680993fc4d6418
vlazed <dinoboy.valdez4@gmail.com>
2024-10-21 15:58:14 -0500

    Ditto

--- lua/weapons/gmod_tool/stools/ragdollpuppeteer.lua
+++ lua/weapons/gmod_tool/stools/ragdollpuppeteer.lua
@@ -827,7 +827,6 @@ local function resizePuppeteerToPuppet(puppeteer, puppet, boneCount, floor)
 				if puppet.PhysBoneOffsets[i] then
 					pMatrix = puppeteer:GetBoneMatrix(puppet.PhysBones[i].parentid)
 				end
-				-- TODO: Add support for Stretchy Resized Ragdolls option
 				if pMatrix then
 					pMatrix:Translate(puppet.PhysBoneOffsets[i])
 					cMatrix:SetTranslation(pMatrix:GetTranslation())
