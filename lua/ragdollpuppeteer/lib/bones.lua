eded5319953c64d080bb1665316cd120a2dcd9f0
vlazed <dinoboy.valdez4@gmail.com>
2025-02-19 19:35:49 -0600

    Fix unmatched source & target physics object with `physmap`s + Add `bones.getPhysMap` function, which maps an entity's physics object to another entity's given that they follow a bone mapping. This function was used to fix an issue where even if the puppet and puppeteer models are the same, their mapped physics objects may differ. For SMH retargeting, this also minimizes perceived distortion between models with different skeleton hierarchy. - Add physobj check to ensure we don't perform any more operations necessary fo

--- lua/ragdollpuppeteer/lib/bones.lua
+++ lua/ragdollpuppeteer/lib/bones.lua
@@ -5,6 +5,7 @@ local subdirectory = "bone_definitions/"
 ---@field definitions table<string, BoneDefinition>
 local bones = {
 	definitions = {},
+	physMaps = {},
 }
 
 if file.IsDir(directory, "LUA") then
@@ -15,6 +16,92 @@ if file.IsDir(directory, "LUA") then
 			table.Merge(bones.definitions, definition)
 		end
 	end
+end
+
+local function getRagdollFromEntity(ent)
+	local ragdoll = ent
+	if ent:GetClass() ~= "prop_ragdoll" then
+		ragdoll = ents.Create("prop_ragdoll")
+		ragdoll:SetModel(ent:GetModel())
+		ragdoll:Spawn()
+	end
+
+	return ragdoll, function()
+		if ent:GetClass() ~= "prop_ragdoll" then
+			ragdoll:Remove()
+		end
+	end
+end
+
+---@param ent Entity
+---@return table<integer, string>
+local function getPhysicsObjectBoneNameMap(ent)
+	local map = {}
+	local ragdoll, cleanupRagdoll = getRagdollFromEntity(ent)
+
+	for i = 0, ragdoll:GetPhysicsObjectCount() - 1 do
+		local boneName = ragdoll:GetBoneName(ragdoll:TranslatePhysBoneToBone(i))
+		map[i] = boneName
+	end
+
+	cleanupRagdoll()
+
+	return map
+end
+
+---@param ent Entity
+---@return table<string, integer>
+local function getBoneNamePhysicsObjectMap(ent)
+	local map = {}
+
+	local ragdoll, cleanupRagdoll = getRagdollFromEntity(ent)
+	for i = 0, ragdoll:GetPhysicsObjectCount() - 1 do
+		local boneName = ragdoll:GetBoneName(ragdoll:TranslatePhysBoneToBone(i))
+		map[boneName] = i
+	end
+
+	cleanupRagdoll()
+
+	return map
+end
+
+---Get a mapping of `from` entity's physics object and `to` entity's physics object, which corresponds to a defined `boneMap`
+---@param from Entity
+---@param to Entity
+---@param boneMap BoneDefinition? A bone mapping `from` entity and `to` entity. If this order is not established then the obtained physMap will be incorrect
+---@return table<integer, integer>?
+function bones.getPhysMap(from, to, boneMap)
+	if not boneMap then
+		return
+	end
+
+	local fromModel = from:GetModel()
+	local toModel = to:GetModel()
+
+	if fromModel == toModel then
+		return
+	end
+
+	if bones.physMaps[fromModel .. toModel] then
+		return bones.physMaps[fromModel .. toModel]
+	end
+
+	local map = {}
+
+	local fromPhysObjToName, toNameToPhysObj = getPhysicsObjectBoneNameMap(from), getBoneNamePhysicsObjectMap(to)
+
+	for i = 0, 31 do
+		local fromName = fromPhysObjToName[i]
+		local toName = boneMap[fromName]
+		if toName then
+			map[i] = toNameToPhysObj[toName]
+		end
+	end
+
+	bones.physMaps[fromModel .. toModel] = map
+	bones.physMaps[toModel .. fromModel] = table.Flip(map)
+
+	return map
 end
 
 ---@param from string
