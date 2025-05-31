7ada741375f922039e518fb3b94964f5ceeab46a
vlazed <dinoboy.valdez4@gmail.com>
2025-05-30 23:14:33 -0500

    Use DPresetSaver for bone map generator to load existing maps. Add refresh for DPresetSaver

--- lua/ragdollpuppeteer/client/derma/presetsaver.lua
+++ lua/ragdollpuppeteer/client/derma/presetsaver.lua
@@ -19,9 +19,21 @@ function PANEL:Init()
 		self:LoadPreset()
 	end
 
+	self.refresh = vgui.Create("DImageButton", self)
+	self.refresh:SetImage("icon16/arrow_refresh.png")
+	self.refresh.DoClick = function()
+		self:RefreshDirectory()
+	end
+
 	self.name = ""
 
+	self.loadJSON = true
 	self:SetTall(45)
+end
+
+---@param state boolean
+function PANEL:SetLoadJSON(state)
+	self.loadJSON = state
 end
 
 function PANEL:SetData(newData)
@@ -32,26 +44,17 @@ function PANEL:OnSavePreset()
 	return {}
 end
 
+function PANEL:SaveCondition()
+	local condition = IsValid(self.entity)
+	return condition, not condition and "No entity selected"
+end
+
 function PANEL:OnSaveSuccess() end
 
 ---@param msg string
 function PANEL:OnSaveFailure(msg) end
 
-function PANEL:SavePreset()
-	if not IsValid(self.entity) then
-		self:OnSaveFailure("No entity selected")
-		return
-	end
-	local data = self:OnSavePreset()
-	if not next(data) then
-		self:OnSaveFailure("Empty data")
-		return
-	end
-	if not self.name then
-		self:OnSaveFailure("Empty save name")
-		return
-	end
-
+function PANEL:SaveFunction(data)
 	local json = util.TableToJSON(data, true)
 	if not file.Exists(self.directory, "DATA") then
 		file.CreateDir(self.directory)
@@ -74,34 +77,67 @@ function PANEL:SavePreset()
 		end
 	end
 
-	-- TODO: Use result from file.Write operation to inform user
-	file.Write(self.directory .. "/" .. name .. ".txt", json)
-	-- if not success then
-	-- 	self:OnSaveFailure("message")
-	-- 	return
-	-- end
-	self:AddChoice(name)
+	local success = file.Write(self.directory .. "/" .. name .. ".txt", json)
+	if not success then
+		self:OnSaveFailure("Failed to write preset")
+		return
+	end
+end
+
+function PANEL:EmptyCheck(data)
+	return next(data)
+end
+
+function PANEL:SavePreset()
+	local condition, reason = self:SaveCondition()
+	if not condition then
+		---@cast reason string
+		self:OnSaveFailure(reason)
+		return
+	end
+	local data = self:OnSavePreset()
+	if not self:EmptyCheck(data) then
+		self:OnSaveFailure("Empty data")
+		return
+	end
+	if not self.name then
+		self:OnSaveFailure("Empty save name")
+		return
+	end
+
+	self:SaveFunction(data)
+
+	self:AddChoice(self.name)
 
 	self:RefreshDirectory()
 end
 
----@param preset PoseOffsetPreset
+---@param preset PoseOffsetPreset|string
 ---@param name string
 function PANEL:OnLoadPreset(preset, name) end
 
+---@param reason string
+function PANEL:OnLoadFail(reason) end
+
 function PANEL:LoadPreset()
-	local name = self.selector:GetSelected()
+	local name, filePath = self.selector:GetSelected()
 	if not name then
-		return
+		return self:OnLoadFail("Empty filename")
 	end
-	if not file.Exists(self.directory, "DATA") or not file.Exists(self.directory .. "/" .. name .. ".txt", "DATA") then
-		return
+	if not file.Exists(self.directory, "DATA") or not file.Exists(filePath, "DATA") then
+		return self:OnLoadFail("File path does not exist: " .. filePath)
 	end
 
-	local json = file.Read(self.directory .. "/" .. name .. ".txt", "DATA")
-	local preset = util.JSONToTable(json)
-	if preset then
-		self:OnLoadPreset(preset, name)
+	local json = file.Read(filePath, "DATA")
+	if self.loadJSON then
+		local preset = util.JSONToTable(json)
+		if preset then
+			self:OnLoadPreset(preset, name)
+		else
+			self:OnLoadFail("Failed to read JSON file")
+		end
+	else
+		self:OnLoadPreset(json, name)
 	end
 end
 
@@ -111,7 +147,9 @@ end
 
 function PANEL:PerformLayout()
 	self.selector:SetPos(0, 0)
-	self.selector:SetSize(self:GetWide(), 20)
+	self.selector:SetSize(self:GetWide() - 20, 20)
+	self.refresh:SetPos(self:GetWide() - 20, 0)
+	self.refresh:SetSize(20, 20)
 
 	self.save:SetPos(0, 25)
 	self.save:SetSize(self:GetWide() / 2 - 20, 20)
@@ -133,15 +171,42 @@ function PANEL:SetDirectory(newDirectory)
 	self.directory = newDirectory
 end
 
+---@param fileName string
+---@return boolean
+function PANEL:RefreshFilter(fileName)
+	return true
+end
+
+---@package
+---@param dir string
+---@param list string[]
+---@return {[1]: string, [2]: string}[]
+function PANEL:recurseDirectory(dir, list)
+	local files, folders = file.Find(dir .. "/*", "DATA")
+	---@cast files string[]
+	---@cast folders string[]
+	for _, f in ipairs(files or {}) do
+		if string.GetExtensionFromFilename(f) == "txt" and self:RefreshFilter(f) then
+			table.insert(list, { f, dir .. "/" .. f })
+		end
+	end
+	if folders and #folders > 0 then
+		for _, folder in ipairs(folders) do
+			self:recurseDirectory(dir .. "/" .. folder, list)
+		end
+	end
+	return list
+end
+
 function PANEL:RefreshDirectory()
 	if not file.Exists(self.directory, "DATA") then
 		file.CreateDir(self.directory)
 	end
 	self.selector:Clear()
 
-	local files = file.Find(self.directory .. "/*.txt", "DATA")
-	for k, file in ipairs(files) do
-		self.selector:AddChoice(string.sub(file, 1, -5))
+	local files = self:recurseDirectory(self.directory, {})
+	for _, fileInfo in ipairs(files) do
+		self.selector:AddChoice(string.sub(fileInfo[1], 1, -5), fileInfo[2])
 	end
 end
 
